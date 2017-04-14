c9ea974ddcea5aa48a48db1cfa90a9b74997bc67
xiao <1023836087@qq.com>
2017-04-14 09:29:19 +0800

    first commit

---
+++ hellow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-14 09:28:20
+# @Author  : xiaosha (mingchuizihuo@gmail.com)
+# @Link    : ${link}
+# @Version : $Id$
+
+'this is xx Module'
+
+import sys
+if __name__ == '__main__':
+	print(123)
